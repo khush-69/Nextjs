d316f1d55e48c2b44574559710695552ee5a32a3
Khush <gadhwalkhush123@gmail.com>
2023-09-15 01:34:22 +0530

    Changes done

--- src/pages/orders/index.tsx
+++ src/pages/orders/index.tsx
@@ -1,16 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 function Order() {
-    const [orders, setOrders] = useState([])
+  const [orders, setOrders] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:8080/orders').then(res => res.json()).then(data => setOrders(data))
-    }, [])
+  useEffect(() => {
+    fetch("http://localhost:8080/orders")
+      .then((res) => res.json())
+      .then((data) => setOrders(data));
+  }, []);
 
-
-    return (
-        <div>
-            <h1 className='text-5xl font-bold p-2 m-10'>Orders</h1>
-            <ul>{orders.map((order: any) => (<li key={order.id} className='text-lg font-bold p-1 m-10'>{order.name}</li>))}</ul>
-        </div>)
+  return (
+    <div>
+      <h1 className="text-5xl font-bold p-2 m-10">Orders</h1>
+      <ul>
+        {orders.map((order: any) => (
+          <li key={order.id} className="text-lg font-bold p-1 m-10">
+            Name = {order.orderedBy} amount: {order.amount}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-export default Order
+export default Order;
