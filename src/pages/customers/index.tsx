c1df655eac0c3108c34827cb3941d43b70841c08
Khush <gadhwalkhush123@gmail.com>
2023-09-15 13:54:03 +0530

    commit

--- src/pages/customers/index.tsx
+++ src/pages/customers/index.tsx
@@ -2,105 +2,95 @@ import React, { useEffect, useState } from "react";
 
 function Customer() {
   const [customers, setCustomers] = useState([]);
-  // const [id, setId] = useState("");
-  // const [customerName, setCustomerName] = useState("");
-  // const [customerEmail, setCustomerEmail] = useState("");
-  // const [customerContactNumber, setCustomerContactNumber] = useState("");
-  // const [customerType, setCustomerType] = useState("");
-
   useEffect(() => {
     fetch("http://localhost:8080/customers")
       .then((res) => res.json())
       .then((data) => setCustomers(data));
   }, []);
-  // async function addCustomer() {
-  //   try {
-  //     const response = await fetch("http://localhost:8080/customers/posts", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify({
-  //         id: parseInt(id),
-  //         name: customerName,
-  //         email: customerEmail,
-  //         contact_number: customerContactNumber,
-  //         customer_type: parseInt(customerType),
-  //       }),
-  //     });
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       console.log("Customer added:", data);
-  //       setCustomerName("");
-  //       setCustomerEmail("");
-  //       setCustomerContactNumber("");
-  //       setCustomerType("");
-  //       window.location.reload();
-  //     } else {
-  //       console.log("Error adding customer");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error:", error);
-  //   }
-  // }
-
+  async function addCustomer() {
+    const name = await document.getElementById("name").value;
+    const email = await document.getElementById("email").value;
+    const contact_number = await document.getElementById("contact_number")
+      .value;
+    const customer_type = await document.getElementById("customer_type").value;
+    const address = await document.getElementById("address").value;
+    fetch("http://localhost:8080/customers/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+        contact_number: contact_number,
+        customer_type: parseInt(customer_type),
+        address: address,
+      }),
+    });
+  }
   return (
     <>
+      <form className="flex-col m-8   gap-2  text-gray-600">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="name"
+          className="m-2"
+        />
+        <br />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="email"
+          className="m-2"
+        />
+        <br />
+        <input
+          type="text"
+          name="contact_number"
+          id="contact_number"
+          placeholder="contact Number"
+          className="m-2"
+        />
+        <br />
+        <input
+          type="text"
+          name="customer_type"
+          id="customer_type"
+          placeholder="customer type"
+          className="m-2"
+        />
+        <br />
+        <input
+          type="text"
+          name="address"
+          id="address"
+          placeholder="address"
+          className="m-2"
+        />
+
+        <br />
+        <button
+          className="button bg-blue-500 m-2"
+          type="button"
+          onClick={() => addCustomer().then(() => window.location.reload())}
+        >
+          Add Customer
+        </button>
+      </form>
       <div>
         <h1 className="text-5xl font-bold p-2 m-10">Customers</h1>
         <ul>
           {customers.map((customer: any) => (
             <li key={customer.id} className="text-lg font-bold p-1 m-10">
-              Name = {customer.name} Email: {customer.email} Contact{" "}
-              {customer.contact_number}
+              id={customer.id}, Name = {customer.name}, Email: {customer.email},
+              Contact {customer.contact_number}
             </li>
           ))}
         </ul>
       </div>
-      {/* <input
-        type="number"
-        name=""
-        value={id}
-        onChange={(e) => setId(e.target.value)}
-        placeholder="Enter id"
-        className="text-black"
-      />
-      <br />
-      <input
-        type="text"
-        name=""
-        value={customerName}
-        onChange={(e) => setCustomerName(e.target.value)}
-        placeholder="Enter name"
-        className="text-black"
-      />
-      <br />
-      <input
-        type="email"
-        name=""
-        value={customerEmail}
-        onChange={(e) => setCustomerEmail(e.target.value)}
-        placeholder="Enter email"
-        className="text-black"
-      />
-      <br />
-      <input
-        type="text"
-        name=""
-        value={customerContactNumber}
-        onChange={(e) => setCustomerContactNumber(e.target.value)}
-        placeholder="Enter Contact_Number"
-        className="text-black"
-      />
-      <br />
-      <input
-        type="number"
-        name=""
-        value={customerType}
-        onChange={(e) => setCustomerType(e.target.value)}
-        placeholder="Enter Customer_type"
-        className="text-black"
-      />
-      <br />
-      <button onClick={addCustomer}>Add Customer</button> */}
     </>
   );
 }
